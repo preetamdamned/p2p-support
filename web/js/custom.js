e42b763039825eca65b52fcb0121b929592b4665
preet_000 <preet_000@TRANSCRYPTOR>
2014-08-18 00:54:30 +0530

    added front end libraries. bootstrap and jquery.

---
+++ web/js/custom.js
@@ -0,0 +1,15 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+$(function (){
+   $("#hoslter").click(function (){
+//        alert("Selected!");
+    $("#opt").css("display","block");
+   }) ;
+   $("#dayScholar").click(function (){
+//        alert("Selected!");
+    $("#opt").css("display","none");
+   }) ;
+});
